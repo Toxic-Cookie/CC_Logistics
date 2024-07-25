35818a185289f669553a7af1f98b201f26a2c0cb
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2024-07-24 19:36:00 -0600

    Seeing if changing the line endings does anything

--- CC_Logistics/Core/Lua/CC_Logistics.lua
+++ CC_Logistics/Core/Lua/CC_Logistics.lua
@@ -1,4 +1,5 @@
-﻿ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
+﻿-- testing
+ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
 ws.send("Hello World")
 print(ws.receive())
 ws.close()
--- CC_Logistics/Core/Lua/startup.lua
+++ CC_Logistics/Core/Lua/startup.lua
@@ -1,14 +1,14 @@
 ﻿local Version = "1.0.0"
 
 if (not fs.exists("basalt.lua")) then
-    shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua")
+	shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua")
 end
 if (fs.exists("startup.lua")) then
-    shell.run("delete startup.lua")
+	shell.run("delete startup.lua")
 end
 shell.run("wget https://raw.githubusercontent.com/Toxic-Cookie/CC_Logistics/master/CC_Logistics/Core/Lua/startup.lua startup.lua")
-if (fs.exists("CC_Logistics.lua")) then
-    shell.run("delete CC_Logistics.lua")
+if (fs.exists("CC_Logistics")) then
+	shell.run("delete CC_Logistics")
 end
 shell.run("wget https://raw.githubusercontent.com/Toxic-Cookie/CC_Logistics/master/CC_Logistics/Core/Lua/CC_Logistics.lua CC_Logistics.lua")
 
