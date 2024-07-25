8ce8f64f3483211143581781f1c1b97ec5cd7aee
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2024-07-24 19:32:02 -0600

    Some hidden character is here apparently

--- CC_Logistics/Core/Lua/CC_Logistics.lua
+++ CC_Logistics/Core/Lua/CC_Logistics.lua
@@ -1,5 +1,4 @@
-﻿-- 8080 for dev. 8181 for prod.
-ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
+﻿ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
 ws.send("Hello World")
 print(ws.receive())
 ws.close()
