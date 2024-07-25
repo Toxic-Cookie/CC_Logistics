60d71e778a4f3c2e61ea42002708a55dc89c4bc8
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2024-07-24 20:07:47 -0600

    More fixes because lua skill issue

--- CC_Logistics/Core/Lua/startup.lua
+++ CC_Logistics/Core/Lua/startup.lua
@@ -3,13 +3,11 @@ local Version = "1.0.0"
 if (not fs.exists("basalt.lua")) then
 	shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua")
 end
-if (fs.exists("startup.lua")) then
-	shell.run("delete startup.lua")
-end
+
+pcall(shell.run("delete startup.lua"))
 shell.run("wget https://raw.githubusercontent.com/Toxic-Cookie/CC_Logistics/master/CC_Logistics/Core/Lua/startup.lua startup.lua")
-if (fs.exists("CC_Logistics.lua")) then
-	shell.run("delete CC_Logistics.lua")
-end
+
+pcall(shell.run("delete CC_Logistics.lua"))
 shell.run("wget https://raw.githubusercontent.com/Toxic-Cookie/CC_Logistics/master/CC_Logistics/Core/Lua/CC_Logistics.lua CC_Logistics.lua")
 
 shell.run("CC_Logistics.lua")
