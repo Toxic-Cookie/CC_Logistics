7a4345f3a3d63308b6e563c987b9a1b2081576db
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2024-07-24 20:13:45 -0600

    Quick skill fix

--- CC_Logistics/Core/Lua/CC_Logistics.lua
+++ CC_Logistics/Core/Lua/CC_Logistics.lua
@@ -2,8 +2,8 @@ local Version = "1.0.0"
 
 -- testing
 ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
-if (ws == nil) then
-	while (ws == nil) do
+if (ws == false or ws == nil) then
+	while (ws == false or ws == nil) do
 		print("Failed to connect. Retrying in 15 seconds.")
 		sleep(15)
 		ws = http.websocket("ws://toxic-cookie.duckdns.org:8080/")
